d7eea15e3fe7b230745afd1a983599ea55c6af67
Trimatix <1JasperLaw@gmail.com>
2025-07-27 08:11:31 +0200

    dynamic codec discovery not working

--- BB/lib/AEPi/src/AEPi/codecs/__init__.py
+++ BB/lib/AEPi/src/AEPi/codecs/__init__.py
@@ -3,10 +3,17 @@ from ..exceptions import CodecLoadException
 
 # Discover codecs
 
-_CODECS = ["EtcPakCodec", "Tex2ImgCodec", "RawCodec"]
+try:
+    import EtcPakCodec
+except CodecLoadException:
+    pass
 
-for codec in _CODECS:
-    try:
-        importlib.import_module(f".{codec}", "AEPi.codecs")
-    except CodecLoadException:
-        pass
+try:
+    import Tex2ImgCodec
+except CodecLoadException:
+    pass
+
+try:
+    import RawCodec
+except CodecLoadException:
+    pass
